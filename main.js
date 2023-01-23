6e76927c7ab50f76654383ffc475298cc3056012
Morphyn95 <alexandre.deslypper@gmail.com>
2023-01-23 09:09:08 +0100

    Pokedex.

---
+++ main.js
@@ -0,0 +1,25 @@
+const grid = document.getElementById('grid')
+
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+.then(response => response.json())
+.then(data => {
+    const results = data.results
+
+    results.forEach((pokemon, index) => {
+        const cell =document.createElement('div')
+        cell.classList.add('item')
+        const pokemonNumber = ++index
+        
+        const img = document.createElement('img')
+        img.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonNumber}.png`
+ 
+        const name = document.createElement('div')
+        const pokemonName = pokemon.name
+        name.innerText = pokemonName
+
+        cell.appendChild(img)
+        cell.appendChild(name)
+        grid.appendChild(cell)
+    });
+})
